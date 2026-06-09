e72718b3e7801b4cfa4e7803b8124bb9048a7e0e
Manvi Shalya <manvishalya840@gmail.com>
2026-06-09 21:31:53 +0530

    [Manvi] Add: Added remaining Control Flow assignments

---
+++ gcrcodebase/leetcodebase/maximum69.java
@@ -0,0 +1,27 @@
+package gcrcodebase.leetcodebase;
+
+import java.util.Scanner;
+
+public class maximum69 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int temp = num;
+        int place = 1;
+        int changeplace = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            if (digit == 6) {
+                changeplace = place;
+            }
+
+            temp /= 10;
+            place *= 10;
+        }
+
+        System.out.println(num + 3 * changeplace);
+    }
+}
---
+++ gcrcodebase/leetcodebase/uglynumber.java
@@ -0,0 +1,24 @@
+package gcrcodebase.leetcodebase;
+
+public class uglynumber {
+    public static void main(String[] args) {
+        int n = 10; 
+        System.out.println("Is " + n + " an ugly number? " + isUgly(n));
+    }
+
+    public static boolean isUgly(int num) {
+        if (num <= 0) {
+            return false; 
+        }
+        
+        int[] factors = {2, 3, 5};
+        
+        for (int factor : factors) {
+            while (num % factor == 0) {
+                num /= factor; 
+            }
+        }
+        
+        return num == 1; 
+    }
+}
